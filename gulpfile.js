3d659d374aa0aad873da959a1f8b87188158715a
Maris Pukitis <mpukitis@rightpoint.com>
2016-04-29 17:20:50 -0500

    Add browser-sync, remove load-plugins, fix for gulp-sass error messages, update dependencies versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,38 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var sass = require('gulp-sass');
+var sass = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
 ];
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('scss/app.scss')
-    .pipe($.sass({
+    .pipe(sass({
       includePaths: sassPaths
     })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
+      .on('error', function (err) {
+        console.log(err.message + ' on line ' + err.lineNumber + ' in file : ' + err.fileName);
+      })
+    )
+    .pipe(autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload); // Watch HTML
 });
